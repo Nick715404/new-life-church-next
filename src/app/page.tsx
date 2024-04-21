c8ed3375c82b26b37dffde3ac3c1a3b6c7763377
nicklllson <nikita.vasia2015@gmail.com>
2024-04-21 22:15:29 +0500

    Update: Сделал главный экран домашней страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
+import styles from './page.module.scss';
+
+import { unbounded } from '@/constants/fonts';
+
 import { VideoBackground } from "@/components/VideoBackground/VideoBackground";
 
 export default function HomePage() {
   return (
-    <div>
-      <VideoBackground
-        src="videos/placeholder.mp4"
-        isAutoPlay
-        isLoop
-        isMuted
-      />
-    </div>
+    <>
+      <section className={styles.hero}>
+        <div className="container">
+          <div className={styles.heroWrapper}>
+            <h1 className={`${unbounded.className} ${styles.heroTitleBox}`}>
+              <span className={styles.heroSubTitle}>Добро пожаловать<br />в церковь</span>
+              <span className={styles.heroTitle}>“Новая жизнь”</span>
+            </h1>
+          </div>
+        </div>
+        <VideoBackground
+          src="videos/placeholder.mp4"
+          isAutoPlay
+          isLoop
+          isMuted
+        />
+      </section>
+
+    </>
   );
 }

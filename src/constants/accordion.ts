af03d52616b40bc8a44b894683b3090b7ffff810
nicklllson <nikita.vasia2015@gmail.com>
2024-06-30 23:35:25 +0500

    Поправил информацию в FAQ в аккордионе на странице Contacts

--- src/constants/accordion.ts
+++ src/constants/accordion.ts
@@ -2,26 +2,26 @@ export const contactsAccordion = [
   {
     id: 1,
     title: 'Могу ли я свободно посетить вашу церковь?',
-    description: 'Nibh sed pulvinar proin gravida hendrerit lectus a. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Cursus metus aliquam.'
+    description: 'Да, конечно! В нашей церки собрания проходят каждое воскресенье в 10:00, 13:00 и так же в 16:00 проходит молодежное служение и на каждое служение вход свободный. В среду в 18:30 проходит молитвенное служение, оно так же открыто для посещения.'
   },
   {
     id: 2,
-    title: 'Могу ли я свободно посетить вашу церковь?',
-    description: 'Nibh sed pulvinar proin gravida hendrerit lectus a. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Cursus metus aliquam.'
+    title: 'Как я могу попасть в какое-либо служение?',
+    description: 'Самым лучшим спосбом будет связаться с лидером служения, в которое вы ходите попасть и пообщаться с ним или указать в форме тему "Вопрос о служении" и вам ответят в ближайешее время.'
   },
   {
     id: 3,
-    title: 'Могу ли я свободно посетить вашу церковь?',
-    description: 'Nibh sed pulvinar proin gravida hendrerit lectus a. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Cursus metus aliquam.'
+    title: 'Проходят ли в церкви домашние группы?',
+    description: 'Да, в цереви Слово Жизни г. Челябинск проходят домашние группы как для взрослого поколения и молодежи, и так же для подростков. Укажите в форме тему "Домашние группы" и ваш более конкретный и индивидуальный вопрос, и с вами свяжутся.'
   },
   {
     id: 4,
-    title: 'Могу ли я свободно посетить вашу церковь?',
-    description: 'Nibh sed pulvinar proin gravida hendrerit lectus a. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Cursus metus aliquam.'
+    title: 'Есть ли в вашей церкви детское служение?',
+    description: 'В нашей церкви проходит детское служение в воскресенье в 10:30 и 13:30. На детском служение проходит работа с детьми от 3 до 11 лет, где им так же рассказывают о христианских истинах и ценностях.'
   },
   {
     id: 5,
-    title: 'Могу ли я свободно посетить вашу церковь?',
-    description: 'Nibh sed pulvinar proin gravida hendrerit lectus a. Consequat mauris nunc congue nisi vitae suscipit tellus mauris a. Cursus metus aliquam.'
+    title: 'Как я могу узнать последние новости в церкви?',
+    description: 'Для этого есть несколько способов. Первый - на главной странице сайта отображен календарь, куда заносятся все актуальные данные о служениях и мероприятиях. Второй - церковь публикует новости в сообщество Вконтакте и Телеграм-канал (ссылки находятся ниже, в подвале сайта). Третий - наш YouTube канал, куда выкладываются актуальные воскресные служения и проповеди наших пасторов.'
   },
 ]

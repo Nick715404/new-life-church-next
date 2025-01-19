ae8309eb51e6216161db5c9e6f0b265d29eef06f
Nick715404 <nikita.vasia2015@gmail.com>
2025-01-19 19:42:18 +0500

    feat

--- src/app/register/bussines/route.ts
+++ src/app/register/bussines/route.ts
@@ -1,22 +1,46 @@
+// route.ts
 'use server';
 
+import { render } from '@react-email/render';
+import Email from '../../../components/emails/ui/BussinesEmail';
 import { NextResponse } from 'next/server';
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+	host: '',
+	port: 0,
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL_USER, // Почта для отправки
+		pass: process.env.EMAIL_PASS, // Пароль или токен для почты
+	},
+});
 
 export async function POST(req: Request) {
 	try {
-		// Читаем тело запроса
-		const body = await req.json();
-		console.log('Received data:', body);
+		const { email, first_name, last_name } = await req.json();
 
-		// Возвращаем успешный ответ
+		console.log('Received data:', {
+			email,
+			first_name,
+			last_name,
+		});
+
+		const html = await render(Email({ text: 'awdawd' }));
+
+		await transporter.sendMail({
+			from: process.env.EMAIL_USER,
+			to: email,
+			subject: 'Your Message',
+			html: html,
+		});
+
 		return NextResponse.json({
 			success: true,
-			message: 'Data received successfully',
+			message: 'Email sent successfully',
 		});
 	} catch (error) {
 		console.error('Error processing request:', error);
-
-		// Возвращаем ошибку
 		return NextResponse.json(
 			{ success: false, error: 'Failed to process request' },
 			{ status: 500 }
---
+++ src/components/emails/ui/BussinesEmail.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function BussinesEmail({ text }: { text: string }) {
+	return (
+		<div className=''>
+			<h1>Hello there!</h1>
+		</div>
+	);
+}

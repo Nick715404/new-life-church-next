e0135bf3d8e636c2a9beabb13945e3066a275379
Nick715404 <nikita.vasia2015@gmail.com>
2025-01-19 20:55:31 +0500

    change some comments

--- src/app/register/bussines/route.ts
+++ src/app/register/bussines/route.ts
@@ -7,12 +7,12 @@ import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-	host: '',
-	port: 0,
+	host: 'smtp.gmail.com',
+	port: 465,
 	service: 'gmail',
 	auth: {
-		user: process.env.EMAIL_USER, // Почта для отправки
-		pass: process.env.EMAIL_PASS, // Пароль или токен для почты
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS,
 	},
 });
 

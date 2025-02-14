461fce423fc28d1f1854cc9cf00b1b9c1c0af230
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-14 11:56:59 +0500

    fix: create onKeyDown for popup close btn

--- src/components/Popup/ui/PopupCloseBtn.tsx
+++ src/components/Popup/ui/PopupCloseBtn.tsx
@@ -1,9 +1,25 @@
+'use client';
+
 import { usePopupContext } from './PopupProvider';
 import styles from '../model/styles.module.scss';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 export const PopupCloseBtn = () => {
 	const { setOpen } = usePopupContext();
+
+	useEffect(() => {
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setOpen!(false);
+			}
+		};
+
+		document.addEventListener('keydown', handleKeyDown);
+		return () => {
+			document.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [setOpen]);
 
 	return (
 		<button

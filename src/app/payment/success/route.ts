95c53bbc867adc7580b8835ad55eb0c063d93fe9
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-17 22:24:06 +0500

    add loging

--- src/app/payment/success/route.ts
+++ src/app/payment/success/route.ts
@@ -9,7 +9,10 @@ import {
 import crypto from 'crypto';
 
 export async function POST(req: Request) {
+	const data = await req.json();
 	const formData = await req.formData();
+
+	console.log(data);
 
 	const outSum = formData.get('OutSum')?.toString();
 	const invId = formData.get('InvId')?.toString();

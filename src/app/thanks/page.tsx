cf0a3b1a88225dd96ead6bdd5ff0e629283769ed
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-17 20:19:28 +0500

    remove logic from thanks page

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -4,27 +4,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { halvar } from '@/constants/fonts';
 import { MotionBox } from '@/components/MotionBox';
-import { eventSwitcher } from '@/utils/register/session';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
 import styles from './page.module.scss';
-import { sendEmail } from '@/utils';
-import { storeManipulate } from '@/utils/local-storage';
 
 export default function ThanksPage() {
-	const [status, setStatus] = useState<'pending' | 'loading' | 'loaded'>(
-		'pending'
-	);
-	const router = useRouter();
-
-	useEffect(() => {
-		const storedData = localStorage.getItem('formData');
-
-		if (storedData) {
-			storeManipulate(storedData, setStatus, router);
-		}
-	}, []);
-
 	return (
 		<main className='thanks children-page'>
 			<div className='container'>
@@ -40,36 +22,19 @@ export default function ThanksPage() {
 						/>
 					</MotionBox>
 					<MotionBox delay={0.2}>
-						{status === 'loading' && (
-							<h1 className={`${styles.title} ${halvar.className}`}>
-								Пожалуйста не покидайте <br /> страницу
-							</h1>
-						)}
-						{status !== 'loading' && (
-							<h1 className={`${styles.title} ${halvar.className}`}>
-								Спасибо за ваше <br /> щедрое сердце
-							</h1>
-						)}
+						<h1 className={`${styles.title} ${halvar.className}`}>
+							Спасибо за ваше <br /> щедрое сердце
+						</h1>
 					</MotionBox>
 					<MotionBox delay={0.3}>
-						{status === 'loading' ? (
-							<p className={styles.text}>
-								Мы регистрируем вас на мероприятие, пожалуйста подождите
-							</p>
-						) : (
-							<p className={styles.text}>
-								Вся информация направлена <br /> вам на почту.
-							</p>
-						)}
+						<p className={styles.text}>
+							Вся информация направлена <br /> вам на почту.
+						</p>
 					</MotionBox>
 					<MotionBox delay={0.4} className={styles.linkBox}>
-						{status === 'loading' ? (
-							<span>Загрузка...</span>
-						) : (
-							<Link className={styles.link} href='/'>
-								На главную
-							</Link>
-						)}
+						<Link className={styles.link} href='/'>
+							На главную
+						</Link>
 					</MotionBox>
 				</div>
 			</div>

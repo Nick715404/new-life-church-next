3c11e5b43491ce5d5a5b2fb817fc960f532ca94b
Nick715404 <nikita.vasia2015@gmail.com>
2025-02-18 18:24:23 +0500

    f;

--- src/app/payment/pay/route.ts
+++ src/app/payment/pay/route.ts
@@ -25,12 +25,12 @@ export async function POST(req: Request) {
 	const data = await req.json();
 
 	const mrhLogin = `${process.env.MRC_LOGIN}`;
-	const mrhPass1 = `${process.env.MRH_PASS_1}`;
+	const mrhPass1 = `${process.env.MRC_PASS_1}`;
 
 	const invId = Math.floor(Math.random() * 1000000); // Уникальный ID заказа
 	const invDesc = 'Добровольное пожертвование'; // Описание заказа
 	const outSum = data.price; // Сумма платежа
-	const isTest = 1;
+	const isTest = 0;
 
 	// Генерация подписи (CRC)
 	const crc = crypto

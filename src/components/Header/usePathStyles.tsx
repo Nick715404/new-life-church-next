12d9aac961706ef2d7d87a6644421ffeab58ca2c
nicklllson <nicklllson.off@gmail.com>
2025-11-22 15:48:45 +0500

    fix: add leaders summit page to white header

--- src/components/Header/usePathStyles.tsx
+++ src/components/Header/usePathStyles.tsx
@@ -1,17 +1,18 @@
 type usePathStylesProps = { pathname: string };
 
 export function usePathStyles({ pathname }: usePathStylesProps) {
-	const whitePaths = [
-		'/',
-		'/donation',
-		'/contacts',
-		'/hello',
-		'/events/youth-ural',
-		'/events/business-conf',
-		'/events/ural-faith-conf',
-		'/events/chel-fire',
-		'/youth-magnitogorsk',
-		'/yearforgod',
-	];
-	return whitePaths.includes(pathname) ? 'white' : 'black';
+  const whitePaths = [
+    '/',
+    '/donation',
+    '/contacts',
+    '/hello',
+    '/events/youth-ural',
+    '/events/business-conf',
+    '/events/ural-faith-conf',
+    '/events/chel-fire',
+    '/youth-magnitogorsk',
+    '/yearforgod',
+    'globalnyj-liderskij-sammit',
+  ];
+  return whitePaths.includes(pathname) ? 'white' : 'black';
 }

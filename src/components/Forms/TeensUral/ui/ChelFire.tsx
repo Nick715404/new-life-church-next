dea7398e44f219f435af5c7ef1836b2a6eab5317
nicklllson <nicklllson.off@gmail.com>
2026-04-06 20:16:22 +0500

    fix: change chel fire homecover quantity

--- src/components/Forms/TeensUral/ui/ChelFire.tsx
+++ src/components/Forms/TeensUral/ui/ChelFire.tsx
@@ -121,7 +121,7 @@ export const ChelFire = () => {
         <FormCheckbox<FormFields>
           name="homeCover"
           register={register}
-          label="Мне нужно расселение (доступно только 83 мест)"
+          label="Мне нужно расселение (доступно только 57 мест)"
         />
         <label htmlFor="agreement" className={styles.checkboxLabel}>
           <input

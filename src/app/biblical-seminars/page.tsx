abcac00d5e94c85c7acb61b54ae31eae360ca90d
nicklllson <nicklllson.off@gmail.com>
2026-06-05 15:30:44 +0500

    feat: add metadata for bs

--- src/app/biblical-seminars/page.tsx
+++ src/app/biblical-seminars/page.tsx
@@ -8,13 +8,22 @@ import {
   BiblicalSchedule,
   BiblicalCta,
 } from '@/sections/biblical-seminars';
+import { Metadata } from 'next';
 
 export const revalidate = false;
 export const dynamic = 'force-static';
 
+export const metadata: Metadata = {
+  title: 'Библейские семинары - Церковь Слово Жизни г. Челябинск',
+  description:
+    'Библейские семинары - это девять месяцев глубокого погружения в Писание, церковную культуру и настоящее христианское комьюнити — для молодёжи, которая хочет строить крепкий духовный фундамент.',
+};
+
 export default function BiblicalSeminarPage() {
   return (
-    <div className={`${oswald.variable} ${manrope.variable} ${caveat.variable} ${styles.page}`}>
+    <div
+      className={`${oswald.variable} ${manrope.variable} ${caveat.variable} ${styles.page}`}
+    >
       <div className={styles.wrap}>
         <BiblicalHero />
         <BiblicalStatement />
